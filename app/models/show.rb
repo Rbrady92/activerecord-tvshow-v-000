2a89aa22937324fd6dfaf30902655676c6d7d780
Ryan Brady <ryanbrady369@gmail.com>
2019-01-29 23:33:01 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,30 @@
 class Show < ActiveRecord::Base
+  def highest_rating
+    Show.maximum(:rating)
+  end
+
+  def most_popular_show
+    highest_rating.name
+  end
+
+  def lowest_rating
+    Show.minimum(:rating)
+  end
+
+  def least_popular_show
+    lowest_rating.name
+  end
+
+  def ratings_sum
+    Show.sum(:ratings)
+  end
+
+  def popular_shows
+    Show.where(:ratings > 5)
+  end
+
+  def shows_by_alphabetical_order
+    Show.all.order(:name :desc)
+  end
+
 end
